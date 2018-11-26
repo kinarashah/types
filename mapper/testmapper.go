8d20d6d32592db22c23822796f874230d213ea13
kinarashah <kinara@rancher.com>
2018-11-26 15:07:34 -0800

    ../

--- mapper/testmapper.go
+++ mapper/testmapper.go
@@ -1,42 +1,38 @@
 package mapper
 
 import (
-	"encoding/json"
 	"github.com/rancher/norman/types"
-	"github.com/rancher/norman/types/convert"
-	"github.com/rancher/norman/types/values"
-	"github.com/sirupsen/logrus"
 )
 
 type TestMapper struct {
 }
 
 func (s TestMapper) FromInternal(data map[string]interface{}) {
-	test := convert.ToString(values.GetValueN(data, "data", "digitaloceancredentialConfig"))
-	if test != "" {
-		ans, _ := json.Marshal(data)
-		logrus.Infof("entered here %v", string(ans))
-
-		delete(data, "data")
-		values.PutValue(data, convert.ToString(test), "digitaloceancredentialConfig", "accessToken")
-
-		ans, _ = json.Marshal(data)
-		logrus.Infof("now from here %v", string(ans))
-	}
+	//test := convert.ToString(values.GetValueN(data, "data", "digitaloceancredentialConfig"))
+	//if test != "" {
+	//	ans, _ := json.Marshal(data)
+	//	logrus.Infof("entered here %v", string(ans))
+	//
+	//	delete(data, "data")
+	//	values.PutValue(data, convert.ToString(test), "digitaloceancredentialConfig", "accessToken")
+	//
+	//	ans, _ = json.Marshal(data)
+	//	logrus.Infof("now from here %v", string(ans))
+	//}
 }
 
 func (s TestMapper) ToInternal(data map[string]interface{}) error {
-	ans, _ := json.Marshal(data)
-	logrus.Infof("entered here %v", string(ans))
-
-	stringData := map[string]string{}
-	stringData["digitaloceancredentialConfig"] = convert.ToString(values.GetValueN(data, "digitaloceancredentialConfig", "accessToken"))
-
-	values.PutValue(data, stringData, "stringData")
-	delete(data, "digitaloceancredentialConfig")
-
-	ans, _ = json.Marshal(data)
-	logrus.Infof("now here %v", string(ans))
+	//ans, _ := json.Marshal(data)
+	//logrus.Infof("entered here %v", string(ans))
+	//
+	//stringData := map[string]string{}
+	//stringData["digitaloceancredentialConfig"] = convert.ToString(values.GetValueN(data, "digitaloceancredentialConfig", "accessToken"))
+	//
+	//values.PutValue(data, stringData, "stringData")
+	//delete(data, "digitaloceancredentialConfig")
+	//
+	//ans, _ = json.Marshal(data)
+	//logrus.Infof("now here %v", string(ans))
 	return  nil
 }
 
