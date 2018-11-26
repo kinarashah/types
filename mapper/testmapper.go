0f9bebbf3c57f77d2c56cc35ec8ad03ca48264fa
kinarashah <kinara@rancher.com>
2018-11-26 14:27:32 -0800

    remove id

--- mapper/testmapper.go
+++ mapper/testmapper.go
@@ -2,7 +2,6 @@ package mapper
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/rancher/norman/types"
 	"github.com/rancher/norman/types/convert"
 	"github.com/rancher/norman/types/values"
@@ -21,7 +20,7 @@ func (s TestMapper) FromInternal(data map[string]interface{}) {
 		delete(data, "data")
 		values.PutValue(data, convert.ToString(test), "digitaloceancredentialConfig", "accessToken")
 
-		values.PutValue(data, fmt.Sprintf("%s:%s", convert.ToString(values.GetValueN(data, "id")), convert.ToString(values.GetValueN(data, "namespaceId"))), "id")
+		//values.PutValue(data, fmt.Sprintf("%s:%s", convert.ToString(values.GetValueN(data, "id")), convert.ToString(values.GetValueN(data, "namespaceId"))), "id")
 		
 		ans, _ = json.Marshal(data)
 		logrus.Infof("now from here %v", string(ans))
