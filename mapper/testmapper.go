6c2dcfe8d4091cd7746f26c943598ff148845d9e
kinarashah <kinara@rancher.com>
2018-11-27 10:50:18 -0800

    nov27

--- mapper/testmapper.go
+++ mapper/testmapper.go
@@ -2,45 +2,68 @@ package mapper
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/rancher/norman/types"
 	"github.com/rancher/norman/types/convert"
 	"github.com/rancher/norman/types/values"
 	"github.com/sirupsen/logrus"
+	"strings"
 )
 
 type TestMapper struct {
 }
 
 func (s TestMapper) FromInternal(data map[string]interface{}) {
-	test := convert.ToString(values.GetValueN(data, "data", "digitaloceancredentialConfig"))
-	if test != "" {
+	config, field, value := getKeys(data)
+	logrus.Infof("config %s field %s value %s", config, field, value)
+
+	if config != "" {
+		values.PutValue(data, value, config, field)
+
 		ans, _ := json.Marshal(data)
-		logrus.Infof("entered here %v", string(ans))
-
-		delete(data, "data")
-		values.PutValue(data, convert.ToString(test), "digitaloceancredentialConfig", "accessToken")
-
-		ans, _ = json.Marshal(data)
-		logrus.Infof("now from here %v", string(ans))
+		logrus.Infof("now internal here %v", string(ans))
 	}
+
+	delete(data, "data")
 }
 
 func (s TestMapper) ToInternal(data map[string]interface{}) error {
 	ans, _ := json.Marshal(data)
 	logrus.Infof("entered here %v", string(ans))
 
-	stringData := map[string]string{}
-	stringData["digitaloceancredentialConfig"] = convert.ToString(values.GetValueN(data, "digitaloceancredentialConfig", "accessToken"))
-
-	values.PutValue(data, stringData, "stringData")
-	delete(data, "digitaloceancredentialConfig")
-
+	updateData(data)
 	ans, _ = json.Marshal(data)
 	logrus.Infof("now here %v", string(ans))
-	return  nil
+	return nil
 }
 
 func (s TestMapper) ModifySchema(schema *types.Schema, schemas *types.Schemas) error {
 	return nil
 }
 
+func updateData(data map[string]interface{}) {
+	stringData := map[string]string{}
+	for key, val := range data {
+		if strings.HasSuffix(key, "Config") {
+			for key2, val := range convert.ToMapInterface(val) {
+				stringData[fmt.Sprintf("%s:%s",key,key2)] = convert.ToString(val)
+				values.PutValue(data, stringData, "stringData")
+				delete(data, key)
+				return
+			}
+		}
+	}
+}
+
+func getKeys(data map[string]interface{}) (string, string, string) {
+	for key, val := range data {
+		splitKeys := strings.Split(key, ":")
+		if len(splitKeys) != 2 {
+			continue
+		}
+		if strings.HasSuffix(splitKeys[0], "Config"){
+			return splitKeys[0], splitKeys[1], convert.ToString(val)
+		}
+	}
+	return "", "", ""
+}
