fcc654bbb9ecf3389d12cfc1112b4a7ddbff9a36
kinarashah <kinara@rancher.com>
2019-09-05 14:39:08 -0700

    test changes

--- apis/management.cattle.io/v3/schema/schema.go
+++ apis/management.cattle.io/v3/schema/schema.go
@@ -76,6 +76,7 @@ func schemaTypes(schemas *types.Schemas) *types.Schemas {
 func credTypes(schemas *types.Schemas) *types.Schemas {
 	return schemas.
 		AddMapperForType(&Version, v3.CloudCredential{},
+			&m.DisplayName{},
 			&mapper.CredentialMapper{},
 			&m.Drop{Field: "namespaceId"}).
 		MustImport(&Version, v3.CloudCredential{})
